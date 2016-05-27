8e186509fc86bff0120333516de2d2c5b647f058
BOZ2323 <lillibaaske@gmx.de>
2016-05-27 13:15:43 +0200

    Add movement

--- virun.js
+++ virun.js
@@ -1,3 +1,11 @@
 console.log("Helllo world");
 var virus = document.querySelector("div");
 virus.style.top = "400px";
+var x = 1;
+var v = .1; // px per millisecond
+var startTime = Date.now();
+setInterval(function(){
+    var t = Date.now() - startTime;    
+    x = v * t;
+    virus.style.left = x + "px";
+},20); // ms  | 1000/20 = 50 frames per second (50 fps)
