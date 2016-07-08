3601eb38f5c3e5b2524c2e27e982e7b3fafa8e02
BOZ2323 <lillibaaske@gmx.de>
2016-07-08 13:28:52 +0200

    Refactoring plus fix double restart when UFO reaches right edge of screen after restartTimer was already set through crash

--- virun.js
+++ virun.js
@@ -1,12 +1,18 @@
 var virus = document.querySelector("div");
-var x = 1;
-var y = 0;
-var vy = 0;
-var ay = 0;
+
+var x;
+var y;
+
 var vx = 0.1; // px per millisecond
+var vy = 0;
+
+var ay = 0;
+
 var startTime = Date.now();
 var clickTime;
-timer = setInterval(function animate() {
+var restartTimer = null;
+
+function animate() {
 	var t = Date.now() - startTime;
 	x = vx * t;
 	y = vy * t + 400;
@@ -15,24 +21,43 @@ timer = setInterval(function animate() {
 	virus.style.top = y + "px";
 
 	if ( x > document.body.clientWidth) {
-		startTime = Date.now();
+        // if our timer will trigger the restart in the future,
+        // we do nothing here and just wait for the timer to fire.
+        if (!restartTimer) {
+            startTime = Date.now();
+        }
 	}
 	if (ay >= 0.001) {
 		t = Date.now() - clickTime;
 		vy = ay * t;
 	}
-	if (y > screen.height){
-		console.log("hello");
-		startTime = setTimeout(function() {
+    
+    /* 
+     * If the UFO has fallen off screen
+     * and we did not start a timer yet,
+     * start one. 
+     * restartTimer gets the setTimeout ID (Number)
+     * as long as the Timeout is running.
+     * Then restartTimer is set to null again
+     */
+	if (y > screen.height && !restartTimer){
+		console.log("gone");
+		restartTimer = setTimeout(function(){ 
+            startTime = Date.now();
+            restartTimer = null;
 			vy = 0;
 			ay = 0;
-		}, 10000);
+		}, 4000);
 	}
-},20); // ms  | 1000/20 = 50 frames per second (50 fps)
+}
 
-virus.addEventListener("click", function onclick(event) {
+function crash(event) {
 	ay = 0.001;
 	clickTime = Date.now();
-});
+}
+
+var timer = setInterval(animate, 20); // ms  | 1000/20 = 50 frames per second (50 fps)
+
+virus.addEventListener("click", crash);
 
 
