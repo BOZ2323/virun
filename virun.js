53a79fd3e8466db84253f8774261bef0d3833a62
BOZ2323 <lillibaaske@gmx.de>
2016-07-08 12:07:13 +0200

    Add setTimeout property to delay ufo before starting over again

--- virun.js
+++ virun.js
@@ -3,45 +3,36 @@ var x = 1;
 var y = 0;
 var vy = 0;
 var ay = 0;
-var vx = .1; // px per millisecond
+var vx = 0.1; // px per millisecond
 var startTime = Date.now();
 var clickTime;
 timer = setInterval(function animate() {
-	var t = Date.now() - startTime;    
+	var t = Date.now() - startTime;
 	x = vx * t;
 	y = vy * t + 400;
-	
+
 	virus.style.left = x + "px";
 	virus.style.top = y + "px";
 
 	if ( x > document.body.clientWidth) {
 		startTime = Date.now();
 	}
-	if (ay >= .001) {
-		var t = Date.now() - clickTime;		
+	if (ay >= 0.001) {
+		t = Date.now() - clickTime;
 		vy = ay * t;
 	}
-	//if ( y > document.body.clientHeight && x > document.body.clientWidth) {
-	//	console.log("hello");
-	//	startTime = Date.now();
-	//}
 	if (y > screen.height){
 		console.log("hello");
-		startTime = Date.now();
-		vy = 0;
-		ay = 0;
+		startTime = setTimeout(function() {
+			vy = 0;
+			ay = 0;
+		}, 10000);
 	}
-	//if ( y > document.body.clientHeight && x > document.body.clientWidth) {
-	//	console.log("second if");
-	//	vy = 0;
-	//	ay = 0;
-//}
 },20); // ms  | 1000/20 = 50 frames per second (50 fps)
 
 virus.addEventListener("click", function onclick(event) {
-	ay = .001;
+	ay = 0.001;
 	clickTime = Date.now();
-
 });
 
 
