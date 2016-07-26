02605e77cd840de69755dd3ad401b3d80180254f
BOZ2323 <lillibaaske@gmx.de>
2016-07-26 13:22:04 +0200

    Tried refactoring, program does not work properly now

--- virun.js
+++ virun.js
@@ -1,64 +1,65 @@
-var virus = document.querySelector("div");
+function fly(time, ay)
+    var virus = document.querySelector("div");
 
-var x;
-var y;
+    var x;
+    var y;
 
-var vx = 0.1; // px per millisecond
-var vy = 0;
+    var vx = 0.1; // px per millisecond
+    var vy = 0;
 
-var ay = 0;
+    var ay = 0;
 
-var startTime = Date.now();
-var clickTime;
-var restartTimer = null;
+    var startTime = Date.now();
+    var clickTime;
+    var restartTimer = null;
 
-function animate() {
-	var t = Date.now() - startTime;
-	x = vx * t;
-	y = vy * t + 400;
+    function animate() {
+        var t = Date.now() - startTime;
+        x = vx * t;
+        y = vy * t + 400;
 
-	virus.style.left = x + "px";
-	virus.style.top = y + "px";
+        virus.style.left = x + "px";
+        virus.style.top = y + "px";
 
-	if ( x > document.body.clientWidth) {
-        // if our timer will trigger the restart in the future,
-        // we do nothing here and just wait for the timer to fire.
-        console.log("end of screen");
-        if (!restartTimer) {
-            startTime = Date.now();
+        if ( x > document.body.clientWidth) {
+            // if our timer will trigger the restart in the future,
+            // we do nothing here and just wait for the timer to fire.
+            console.log("end of screen");
+            if (!restartTimer) {
+                startTime = Date.now();
+            }
         }
-	}
-	if (ay >= 0.001) {
-		t = Date.now() - clickTime;
-		vy = ay * t;
-	}
-    
-    /* 
-     * If the UFO has fallen off screen
-     * and we did not start a timer yet,
-     * start one. 
-     * restartTimer gets the setTimeout ID (Number)
-     * as long as the Timeout is running.
-     * Then restartTimer is set to null again
-     */
-	if (y > screen.height && !restartTimer){
-		console.log("gone");
-		restartTimer = setTimeout(function(){ 
-            startTime = Date.now();
-            restartTimer = null;
-			vy = 0;
-			ay = 0;
-		}, 4000);
-	}
+        if (ay >= 0.001) {
+            t = Date.now() - clickTime;
+            vy = ay * t;
+        }
+        
+        /* 
+         * If the UFO has fallen off screen
+         * and we did not start a timer yet,
+         * start one. 
+         * restartTimer gets the setTimeout ID (Number)
+         * as long as the Timeout is running.
+         * Then restartTimer is set to null again
+         */
+        if (y > screen.height && !restartTimer){
+            console.log("gone");
+            restartTimer = setTimeout(function(){ 
+                startTime = Date.now();
+                restartTimer = null;
+                vy = 0;
+                ay = 0;
+            }, time);
+        }
+    }
+
+    function crash(event) {
+        ay = 0.001;
+        clickTime = Date.now();
+    }
+
+    var timer = setInterval(animate, 20); // ms  | 1000/20 = 50 frames per second (50 fps)
+
+    virus.addEventListener("click", crash);
 }
-
-function crash(event) {
-	ay = 0.001;
-	clickTime = Date.now();
-}
-
-var timer = setInterval(animate, 20); // ms  | 1000/20 = 50 frames per second (50 fps)
-
-virus.addEventListener("click", crash);
-
-
+fly(1000,0,5);
